b848fc80c0822f2a7785625aa3b17d496ff99bb3
Esteban Baron <estebanbaron.pro@gmail.com>
2024-05-14 14:19:27 +0200

    feat: add new collection in mongodb file
    Signed-off-by: Esteban Baron <estebanbaron.pro@gmail.com>

--- SparkD05/src/main/scala/MongoDB_esteban_baron.scala
+++ SparkD05/src/main/scala/MongoDB_esteban_baron.scala
@@ -1,6 +1,7 @@
 package polytech.umontpellier.fr
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.lit
 
 object MongoDB_esteban_baron {
   def main(args: Array[String]): Unit = {
@@ -14,8 +15,14 @@ object MongoDB_esteban_baron {
     // Lire un élément de la collection "twitter" de la base de données "do5"
     val df = spark.read.format("mongodb").option("database", "do5").option("collection", "twitter").load()
 
-    // tests
+    // Afficher un élément de la dataframe
     df.show(1)
+
+    // Mettre tous les champs tweet_count égaux à 1
+    val dfUpdated = df.withColumn("tweet_count", lit(1))
+
+    // Écrire les données mises à jour dans MongoDB
+    dfUpdated.write.format("mongodb").option("database", "do5").option("collection", "twitter_updated").mode("append").save()
 
     spark.stop()
   }
