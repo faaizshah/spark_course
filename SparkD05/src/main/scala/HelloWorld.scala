806f29b9b0a145a07a3cd962ad04d4fb6be93368
Arsène Fougerouse <arsene582@gmail.com>
2024-05-14 10:21:26 +0200

    feat: exercise 1
    Signed-off-by: Arsène Fougerouse <arsene582@gmail.com>

--- SparkD05/src/main/scala/HelloWorld.scala
+++ SparkD05/src/main/scala/HelloWorld.scala
@@ -1,7 +1,12 @@
 package polytech.umontpellier.fr
 
 object HelloWorld {
+
+  def printSquare(x: Int) = println(x*x)
+  def printCube(x:Int) = println(x*x*x)
   def main(args: Array[String]): Unit = {
     println("Hello world!")
+    printSquare(3)
+    printCube(5)
   }
 }
