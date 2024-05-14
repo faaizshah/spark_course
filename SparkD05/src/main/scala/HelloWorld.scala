f3012fed662d4a925a7f55fe001cabbd4cb9b817
Simon Lucido <simon34280@gmail.com>
2024-05-14 08:31:19 +0200

    feat: assignement part 1
    Signed-off-by: Simon Lucido <simon34280@gmail.com>

--- SparkD05/src/main/scala/HelloWorld.scala
+++ SparkD05/src/main/scala/HelloWorld.scala
@@ -1,7 +1,18 @@
 package polytech.umontpellier.fr
 
 object HelloWorld {
+  def printSquare(x: Int) = println(x * x)
+  def printCube(x: Int) = println(x * x * x)
+
+  def applyFunctionToNumber(x: Int, func: Int => Unit): Unit = {
+    func(x)
+  }
+
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    printSquare(5)
+    printCube(5)
+
+    applyFunctionToNumber(5, printSquare)
+    applyFunctionToNumber(5, printCube)
   }
 }
