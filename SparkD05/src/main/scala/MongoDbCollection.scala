0702948a1efe53d04cbfb31fed7c32332658c0ed
Arsène Fougerouse <arsene582@gmail.com>
2024-05-14 17:15:26 +0200

    feat: mongodb collector
    Signed-off-by: Arsène Fougerouse <arsene582@gmail.com>

--- SparkD05/src/main/scala/MongoDbCollection.scala
+++ SparkD05/src/main/scala/MongoDbCollection.scala
@@ -1,5 +1,26 @@
 package polytech.umontpellier.fr
 
+import org.apache.spark.sql.SparkSession
+
 object MongoDbCollection {
+
+  ²def main(args: Array[String]) = {
+    val MONGODB_URI = "mongodb://root:my_secret_password@localhost:2707"
+    val spark = SparkSession.builder().appName("Connection test").config("spark.mongodb.read.connection.uri", MONGODB_URI).config("spark.mongodb.write.connection.uri", MONGODB_URI).getOrCreate()
+
+    spark.sparkContext.setLogLevel("DEBUG")
+    val df = spark.read.format("mongodb").option("database", "Spark").option("collection", "tweets").load()
+
+    println(df.count())
+
+    val limited = df.drop("_id").limit(500)
+
+    limited.write
+      .format("mongodb")
+      .mode("append")
+      .option("database", "Spark")
+      .option("collection", "tweets")
+      .save()
+  }
 
 }
